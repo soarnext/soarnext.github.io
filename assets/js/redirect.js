d9d4edbbae8702a5e2c513f5462577db200d9e5d
soarnext <xiaosoar@163.com>
2025-07-13 09:37:24 +0800

    更新识别逻辑

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -21,31 +21,30 @@
             const isQQ = ua.indexOf('qq') !== -1;
             const isMobile = /iphone|ipad|ipod|android/.test(ua);
 
-            if (isWeixin ) {
+            if (isWeixin || isQQ) {
                 let instructionHTML = '<h2>请在外部浏览器中打开</h2>';
-                if (isMobile) {
-                    instructionHTML += '<p>1.点击右上角的菜单按钮(通常是三个点)<br>2.选择<i class="fas fa-globe"></i>“用浏览器打开”</p>';
-                } else {
-                    instructionHTML += `<a href="${window.location.href}" target="_blank">
-                                        <div style="margin-bottom: 1em; font-size: 48px;"><i class="fas fa-globe"></i></div>
-                                        <p>1.点击图标或复制网址，<br>2.在您电脑的浏览器中打开。</p>
-                                     </a>`;
+                if(isWeixin){
+                    if (isMobile) {
+                        instructionHTML += '<p>1.点击右上角的菜单按钮(通常是三个点)<br>2.选择<i class="fas fa-globe"></i>“用浏览器打开”</p>';
+                    } else {
+                        instructionHTML += `<a href="${window.location.href}" target="_blank">
+                                            <div style="margin-bottom: 1em; font-size: 48px;"><i class="fas fa-globe"></i></div>
+                                            <p>请点击图标或复制网址，<br>并在您电脑的浏览器中打开。</p>
+                                         </a>`;
+                    }
+                    document.getElementById('tips').innerHTML = instructionHTML;}
+                else{
+                    if (isMobile) {
+                        instructionHTML += '<p>1.点击右上角的菜单按钮(通常是三个点)<br>2. 选择<i class="fas fa-globe">“浏览器"</i><br>如果没有菜单按钮请复制链接至浏览器打开</p>';
+                    } else {
+                        instructionHTML += `<a href="${window.location.href}" target="_blank">
+                                            <div style="margin-bottom: 1em; font-size: 48px;"><i class="fas fa-globe"></i></div>
+                                            <p>请点击图标或复制网址，<br>并在您电脑的浏览器中打开。</p>
+                                         </a>`;
+                    }
+                    document.getElementById('tips').innerHTML = instructionHTML;
                 }
-                document.getElementById('tips').innerHTML = instructionHTML;
-            } 
-            if(isQQ){
-                    let instructionHTML = '<h2>请在外部浏览器中打开</h2>';
-                if (isMobile) {
-                    instructionHTML += '<p>1.点击右上角的菜单按钮(通常是三个点)<br>2. 选择<i class="fas fa-globe">“浏览器"</i><br>如果没有菜单按钮请复制链接至浏览器打开</p>';
-                } else {
-                    instructionHTML += `<a href="${window.location.href}" target="_blank">
-                                        <div style="margin-bottom: 1em; font-size: 48px;"><i class="fas fa-globe"></i></div>
-                                        <p>请点击图标或复制网址，<br>并在您电脑的浏览器中打开。</p>
-                                     </a>`;
-                }
-                document.getElementById('tips').innerHTML = instructionHTML;
-            } 
-            else {
+            } else {
                 const WORKER_URL = 'https://dl.api.yxc.us.kg/';
                 fetch(`${WORKER_URL}${shortId}`)
                     .then(response => {
