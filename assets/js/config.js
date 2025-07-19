720ba7fc4e9c9b57c4523365c8e1837842fbf601
XiaoSoar <105652758+xiaosoar@users.noreply.github.com>
2025-07-19 16:45:38 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // API Configuration
 export const WORKER_URL = 'https://dl.api.yxc.us.kg/';
 export const GITHUB_PAGES_URL = 'https://dl.chn-hw.eu.org/';
-export const CAP_API_ENDPOINT = 'https://cap.shandian.eu.org/5c701606c2/';
+export const CAP_API_ENDPOINT = 'https://cap.shandian.eu.org/2c365d3c52/';
