046b82b9088926ce29815766ab3ac5fd5d1aff5e
soarnext <xiaosoar@163.com>
2025-07-24 18:39:34 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // API Configuration
 export const WORKER_URL = 'https://dl.api.yxc.us.kg/';
 export const GITHUB_PAGES_URL = 'https://dl.xsoar.cfd/';
-export const CAP_API_ENDPOINT = 'https://cap.shandian.eu.org/8be1f03313/';
+export const CAP_API_ENDPOINT = 'https://cap.shandian.eu.org/7005162e84/';
