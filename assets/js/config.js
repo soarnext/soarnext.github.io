4dca2ade4fc4e98170c94276aa14473a9b49a4f4
xiaosoar <2025802935@qq.com>
2025-07-28 10:48:08 +0800

    1

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // API Configuration
 export const WORKER_URL = 'https://dl.api.yxc.us.kg/';
-export const GITHUB_PAGES_URL = 'https://dl.chn-hw.eu.org/';
+export const GITHUB_PAGES_URL = 'https://dl.xsoar.cfd/';
 export const CAP_API_ENDPOINT = 'https://cap.shandian.eu.org/3cdb5a3793/';
